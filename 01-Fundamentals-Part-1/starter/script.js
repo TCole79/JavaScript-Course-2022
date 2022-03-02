9c547fd1322a8c1c2e482f031727d0de62bdb1c9
TristanCole <tristanleighcole@googlemail.com>
2022-03-02 10:25:09 -0700

    Updated part 1 code to follow tutorial

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,44 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 let firstName = "Tristan";
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = 'Yes!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null); // this is a bug, and was not fixed for legacy reasons
+
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas * 2, ageSarah / 10, 2 ** 3); 
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Tristan";
+const lastName = "cole";
+console.log( firstName + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x =+ 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--; // x = x - 1 = 100
+
+// Comparison operators
+*/
+
