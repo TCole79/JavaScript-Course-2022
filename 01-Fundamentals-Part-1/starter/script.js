61ed86cbd14177c48279e39a1849dd54bc63355a
TristanCole <tristanleighcole@googlemail.com>
2022-03-02 17:15:40 -0700

    Complete type conversion and coercion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -52,7 +52,7 @@ const year = 2022;
 const tristan = `My name is ${firstName} ${lastName}, I'm a junior ${job} and I'm ${year - birthyear} years old`;
 
 
-// Decisions - If statements
+//// Decisions - If statements ////
 const age = 19;
 const isOldEnough = age >= 18;
 
@@ -79,3 +79,22 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 
+
+//// Type Conversion ////
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Tristan"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//// Type Coercion ////
+console.log('I am ' + 23 + ' years old');
+console.log('23' + '10' + 3);
+console.log('23' * '2');
+
+let n = '1' + 1; // 11
+n = n - 1; // 10
+console.log(n);
